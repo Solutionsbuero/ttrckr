149c06b0385eec45ab39bea4cf2e80993714ef5a
72nd <msg@frg72.com>
2020-11-28 15:32:20 +0100

    basic code structure and manual focus

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # ttrckr
 
-Tinfoil Colloquy's barcode based Train Tracking.
+Tinfoil Colloquy's barcode based Train Tracking. This application sources the video-stream from the camera by using OpenCV. This way, the focus of the camera can be manually adjusted. The scanning is done using the [ZBar Library](http://zbar.sourceforge.net/).
 
 
 ## Installation
@@ -8,3 +8,13 @@ Tinfoil Colloquy's barcode based Train Tracking.
 This program relies on the OpenCV Library. For convenience this project uses the unofficial pre-built OpenCV binaries from [opencv-python](https://pypi.org/project/opencv-python/).
 
 _Raspberry Pi:_ You have to add the [Piwheels](https://www.piwheels.org/) Wheel Repository to your system to obtain the OpenCV Binaries for your system.
+
+_All Systems:_ Make sure you have the ZBar library installed:
+
+```shell script
+sudo apt-get install libzbar0
+```
+
+## Usage
+
+You can change the parameters with the constants in the `ttrckr.py` file. To determine the correct focus you can increase/decrease the focus value by five pressing i/d key in the active output window.
--- ttrckr.py
+++ ttrckr.py
@@ -1,4 +1,61 @@
 #!/usr/bin/env python3
 
+CAM_NUMBER = 4
+FOCUS = 0
+
+import cv2
+
+class Tracker:
+    cam = None
+    # Set focus values: 0..255, increment: 5
+    focus = 0
+    show_window = False
+
+    # Webcam number: /etc/videoN.
+    def __init__(self, cam_number, focus, show_window = False):
+        self.cam = cv2.VideoCapture(cam_number)
+        self.focus = focus
+        self.show_window = show_window
+
+    def run(self):
+        self.cam.set(cv2.CAP_PROP_AUTOFOCUS, 0)
+        self.__set_focus(self.focus)
+        while True:
+            ret_val, img = self.cam.read()
+            if self.show_window:
+                if self.__window_output(img):
+                    return
+
+    def __window_output(self, img):
+        cv2.imshow('ttrckr', img)
+        key = cv2.waitKey(1)
+        if key == 27:
+            cv2.destroyAllWindows()
+            return True
+        elif key == 105:
+            self.__set_focus(self.focus + 5)
+            print(f"increase focus to {self.focus}")
+        elif key == 100:
+            self.__set_focus(self.focus - 5)
+            print(f"decrease focus to {self.focus}")
+        pass
+
+    def __set_focus(self, value):
+        if value < 0:
+            print(f"requested focus value {value} is below 0")
+            return
+        elif value > 255:
+            print(f"requested focus value {value} is above 255")
+            return
+        elif value % 5 != 0:
+            print(f"requested focus value {value} ha to be a multiple of 5")
+            return
+        self.cam.set(28, value)
+        self.focus = value
+
+
 if __name__ == "__main__":
-    print("hoi")
+    tracker = Tracker(CAM_NUMBER, FOCUS, show_window=True)
+    print("Press <esc> in the window to exit.")
+    tracker.run()
+
